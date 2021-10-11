11189a72ef56ce828c3f3cbdbf28a873850a3f1e
MiguelGil1 <miguel.gil1@udea.edu.co>
2021-10-10 19:06:17 -0500

    Se comienza a desarrollar la clase planeta

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+#define PATH_SOLAR_SYSTEM "../lab_6/systems/sistema.txt"
+
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
+
+    scene = new QGraphicsScene(this);
+
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->show();
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow(){
     delete ui;
 }
 
